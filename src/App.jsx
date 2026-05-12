48bcef7b2a22eeb7f94ef86a584ae9dd47ec219d
Sveta <sveta.kaloshkina@gmail.com>
2026-05-13 01:33:24 +0300

    phone fix

--- src/App.jsx
+++ src/App.jsx
@@ -112,6 +112,7 @@ export default function App() {
                   {showBanner && (
                     <motion.div
                       key="banner"
+                      className="welcome-banner"
                       initial={{ opacity: 0, y: -8 }}
                       animate={{ opacity: 1, y: 0 }}
                       exit={{ opacity: 0, y: -8 }}
